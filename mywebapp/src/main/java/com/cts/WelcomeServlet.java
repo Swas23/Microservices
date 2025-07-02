041a8b94fb941102fd745ecde782026d983db3aa
Swas23 <swasthikgowda82@gmail.com>
2025-07-02 15:03:59 +0530

    added mywebapp

---
+++ mywebapp/src/main/java/com/cts/WelcomeServlet.java
@@ -0,0 +1,23 @@
+package com.cts;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class WelcomeServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		resp.setContentType("Text/html");
+		PrintWriter out = resp.getWriter();
+		out.println("<h1> Hiii Everyone !!!!!!!!! <h1> ");
+	}
+	
+	
+
+}
